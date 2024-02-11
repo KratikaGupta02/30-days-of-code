7bd3dceddb07280cb214c62c370ad23924086bd7
KratikaGupta02 <158499173+KratikaGupta02@users.noreply.github.com>
2024-02-11 14:02:50 +0530

    Create day11csi.c

---
+++ day11csi.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int binarySearch(int arr[], int low, int high, int target) {
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] > target)
+            return binarySearch(arr, low, mid - 1, target);
+
+        return binarySearch(arr, mid + 1, high, target);
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int target;
+    printf("Enter the integer you want to search: ");
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1)
+        printf("Element not found\n");
+    else
+        printf("Element found at index %d\n", result);
+
+    return 0;
+}
